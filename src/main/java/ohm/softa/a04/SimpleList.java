62fccbd27b54aa84acee5b0ab153ebf9ff27e6c9
nico <->
2023-04-04 10:23:08 +0200

    addDefault

--- src/main/java/ohm/softa/a04/SimpleList.java
+++ src/main/java/ohm/softa/a04/SimpleList.java
@@ -11,6 +11,19 @@ public interface SimpleList<T> extends Iterable<T> {
 	 */
 	int size();
 
+	// Keine Ahnung warum man das tun sollte, aber es geht...
+	@SuppressWarnings("unchecked")
+	default void addDefault(Class<T> clazz) {
+		try {
+			/* better solution would be to use Google Guava to get a type token
+			and use this token to create a new instance
+			because we didn't include a reference to Guava this is the easiest way to create new instance of T */
+			this.add(clazz.newInstance());
+		} catch (InstantiationException | IllegalAccessException e) {
+			e.printStackTrace();
+		}
+	}
+
 	/**
 	 * Generate a new list using the given filter instance.
 	 * @return a new, filtered list
